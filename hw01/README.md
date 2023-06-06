d829192e1c6fa0d3e893dd001043b68a19c543b7
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-06-06 15:43:27 +0300

    add: readme with commands

---
+++ hw01/README.md
@@ -0,0 +1,8 @@
+#CLI
+
+##Commands
+
+- [list](https://monosnap.com/file/A9saLumQbhUFFfdtDvttsa8j3EUZP7)
+- [get](https://monosnap.com/file/HO9FfqNObbmIVzDK0j6aS7iccA0DFS)
+- [add](https://monosnap.com/file/0fqO03BI6kmrt0M4wSf4vo5GMcOGXH)
+- [remove](https://monosnap.com/file/7R9ITTAoPwZ2ks4jZFRE9AO1T5DKjn)
--- hw01/index.js
+++ hw01/index.js
@@ -5,19 +5,19 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
         case 'list': 
             const allContacts = await contacts.listContacts();
-            return console.log(allContacts);
+            return console.table(allContacts);
         
         case 'get': 
             const contact = await contacts.getContactById(id);
-            return console.log(contact);    
+            return console.table(contact);    
         
         case 'add': 
             const newContact = await contacts.addContact({name, email, phone});
-            return console.log(newContact);  
+            return console.table(newContact);  
 
         case 'remove': 
             const deleteContact = await contacts.removeContact(id);
-            return console.log(deleteContact); 
+            return console.table(deleteContact); 
         default:
             console.warn("\x1B[31m Unknown action type!");
     }
